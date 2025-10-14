a78ac265db93f078622a2841eda6377eee315102
Shubham Sharma <shubhamsharma@Shubhams-MacBook-Air.local>
2025-10-14 18:22:10 +0530

    modified code to make it more generic

--- communication-mechanisms/server-sent-events/go.mod
+++ communication-mechanisms/server-sent-events/go.mod
@@ -1,3 +1,7 @@
 module server-sent-events
 
 go 1.23.8
+
+require github.com/google/uuid v1.6.0
+
+require github.com/gorilla/mux v1.8.1
--- communication-mechanisms/server-sent-events/main.go
+++ communication-mechanisms/server-sent-events/main.go
@@ -8,21 +8,30 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", serveIndex)
-	http.HandleFunc("/deployment", serveDeployment)
-	http.HandleFunc("/start", startDeployment)
-	http.HandleFunc("/events", sseHandler)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	router := mux.NewRouter()
+	router.HandleFunc("/", serveIndex)
+	router.HandleFunc("/deployment/{deploymentId}", serveDeployment)
+	router.HandleFunc("/start", startDeployment)
+	router.HandleFunc("/events/{deploymentId}", sseHandler)
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	log.Println("Server started at :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
 func serveIndex(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving index.html")
 	http.ServeFile(w, r, "./static/index.html")
+	log.Println("Served index.html")
 }
 
 func serveDeployment(w http.ResponseWriter, r *http.Request) {
@@ -31,7 +40,10 @@ func serveDeployment(w http.ResponseWriter, r *http.Request) {
 
 func startDeployment(w http.ResponseWriter, r *http.Request) {
 	// Create or truncate messages.txt
-	f, err := os.Create("messages.txt")
+	uuid, _ := uuid.NewUUID()
+	uuidStr := uuid.String()
+	fmt.Println("Starting deployment with UUID:", uuidStr)
+	f, err := os.Create(fmt.Sprintf("deployments/%s.txt", uuid))
 	if err != nil {
 		http.Error(w, "Unable to start deployment", 500)
 		return
@@ -42,15 +54,15 @@ func startDeployment(w http.ResponseWriter, r *http.Request) {
 		for i := 1; i <= 1000; i++ {
 			time.Sleep(100 * time.Millisecond)
 			msg := fmt.Sprintf("Step %d completed: %d\n", i, rand.Intn(1000))
-			appendMessage(msg)
+			appendMessage(msg, uuidStr)
 		}
-		appendMessage("Deployment finished!\n")
+		appendMessage("Deployment finished!\n", uuidStr)
 	}()
-	http.Redirect(w, r, "/deployment", http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/deployment/%s", uuidStr), http.StatusSeeOther)
 }
 
-func appendMessage(msg string) {
-	f, err := os.OpenFile("messages.txt", os.O_APPEND|os.O_WRONLY, 0644)
+func appendMessage(msg string, fileName string) {
+	f, err := os.OpenFile(fmt.Sprintf("deployments/%s.txt", fileName), os.O_APPEND|os.O_WRONLY, 0644)
 	if err == nil {
 		defer f.Close()
 		f.WriteString(msg)
@@ -66,11 +78,20 @@ func sseHandler(w http.ResponseWriter, r *http.Request) {
 	2. Cache-Control: This should be set to no-cache to prevent the browser from caching the event stream.
 	3. Connection: This should be set to keep-alive to keep the connection open for continuous data flow.
 	**/
+
+	deploymentId := mux.Vars(r)["deploymentId"]
+	if deploymentId == "" {
+		http.Error(w, "Deployment ID is required", http.StatusBadRequest)
+		return
+	}
+
+	filePath := fmt.Sprintf("deployments/%s.txt", deploymentId)
+
 	w.Header().Set("Content-Type", "text/event-stream")
 
 	lastLine := 0
 	for {
-		file, err := os.Open("messages.txt")
+		file, err := os.Open(filePath)
 		if err != nil {
 			time.Sleep(1 * time.Second)
 			continue
@@ -90,7 +111,7 @@ func sseHandler(w http.ResponseWriter, r *http.Request) {
 			}
 			lastLine++
 		}
-		time.Sleep(5 * time.Second)
+		time.Sleep(1 * time.Second)
 		fmt.Println("Checking for new messages. Last line:", lastLine)
 	}
 }
